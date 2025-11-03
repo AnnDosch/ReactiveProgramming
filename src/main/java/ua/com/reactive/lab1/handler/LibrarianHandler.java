31bf49636849948982bd6399a2bf2ac3d771afce
AnnDosch <127093447+AnnDosch@users.noreply.github.com>
2025-11-03 13:36:54 +0200

    LibrarianHandler.java

--- src/main/java/ua/com/reactive/lab1/handler/LibrarianHandler.java
+++ src/main/java/ua/com/reactive/lab1/handler/LibrarianHandler.java
@@ -11,14 +11,10 @@ import ua.com.reactive.lab1.entity.Librarian;
 @Component
 public class LibrarianHandler {
 
-    /**
-     * Отримати список усіх бібліотекарів.
-     * GET /api/librarians
-     */
     public Mono<ServerResponse> getAllLibrarians(ServerRequest request) {
 
         Flux<Librarian> librarians = Flux.just(
-                // Додані дані для Librarian
+    
                 new Librarian(10L, "Oksana", "Lytvyn", "Senior Librarian"),
                 new Librarian(11L, "Ihor", "Kravchenko", "Junior Assistant"),
                 new Librarian(12L, "Natalija", "Sydorenko", "Chief Librarian")
